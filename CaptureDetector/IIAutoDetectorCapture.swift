94a8ebff947de2788e5dbb8f64b8ecfd3fa15be0
iceboxi <iceboxi@Adolph-MBPR.local>
2018-05-23 22:55:41 +0800

    first version can detector rectangle smooth

---
+++ CaptureDetector/IIAutoDetectorCapture.swift
@@ -0,0 +1,13 @@
+//
+//  IIAutoDetectorCapture.swift
+//  CaptureDetector
+//
+//  Created by Adolph on 2018/5/23.
+//  Copyright © 2018年 iceboxi. All rights reserved.
+//
+
+import UIKit
+
+class IIAutoDetectorCapture: NSObject {
+
+}
